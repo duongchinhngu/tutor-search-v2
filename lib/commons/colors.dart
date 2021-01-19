6042d6d8614e9d256e5e70083646e44cde9546cd
DuongChinhNgu <duongchinhngu@gmail.com>
2021-01-19 12:01:29 +0700

    styles and colors; udpate hiechiary

--- lib/commons/colors.dart
+++ lib/commons/colors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const mainColor = const Color(0xff0D6EEC);
 const backgroundColor = Colors.white;
 var textGreyColor = const Color(0xff707070);
+const textWhiteColor = Colors.white;
---
+++ lib/commons/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const boxShadowStyle = BoxShadow(
+  color: Colors.grey,
+  blurRadius: 5,
+  offset: Offset(0, 3),
+);
+const headerStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+  color: head
+);
+const textStyle = [];
+const titleStyle = [];
