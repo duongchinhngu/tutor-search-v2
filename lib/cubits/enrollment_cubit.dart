3e8a3fe26cf7f0c5db783c9e227c4d6e63bda763
HuyNT3103 <HuyNTSE63450@fpt.edu.vn>
2021-04-09 16:55:01 +0700

    add course enrollment

--- lib/cubits/enrollment_cubit.dart
+++ lib/cubits/enrollment_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:tutor_search_system/models/enrollment.dart';
+import 'package:tutor_search_system/models/enrollment_course.dart';
 import 'package:tutor_search_system/repositories/enrollment_repository.dart';
+import 'package:tutor_search_system/states/courseenrollment_state.dart';
 import 'package:tutor_search_system/states/enrollment_state.dart';
 
 class EnrollmentCubit extends Cubit<EnrollmentState> {
@@ -18,5 +20,14 @@ class EnrollmentCubit extends Cubit<EnrollmentState> {
     }
   }
 
-  
+  Future getEnrollmentOfMonthByTutorIdDate(
+      int tutorId, String currentdate) async {
+    try {
+      CourseEnrollment courseEnrollment = await _repository
+          .fetchCourseEnrollmentByTutorIdForMonth(tutorId, currentdate);
+      emit(CourseEnrollmentListLoadedState(courseEnrollment));
+    } catch (e) {
+      emit(CourseEnrollmentLoadFailedState('$e'));
+    }
+  }
 }
---
+++ lib/models/enrollment_course.dart
@@ -0,0 +1,37 @@
+class CourseEnrollment {
+  final int id;
+  final int tuteeId;
+  final int courseId;
+  final String description;
+  String status;
+  String createdDate;
+  double studyFee;
+  String courseName;
+
+  CourseEnrollment.modelContructor(this.id, this.tuteeId, this.courseId,
+      this.description, this.status, this.createdDate);
+
+  CourseEnrollment({
+    this.id,
+    this.tuteeId,
+    this.courseId,
+    this.description,
+    this.status,
+    this.studyFee,
+    this.createdDate,
+    this.courseName,
+  });
+
+  factory CourseEnrollment.fromJson(Map<String, dynamic> json) {
+    return CourseEnrollment(
+      id: json['id'],
+      courseId: json['courseId'],
+      courseName: json['courseName'],
+      createdDate: json['createdDate'],
+      description: json['description'],
+      status: json['status'],
+      studyFee: json['studyFee'],
+      tuteeId: json['tuteeId'],
+    );
+  }
+}
